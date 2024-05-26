834c9943e0c0cc86c25f42aa976f69665b673437
Ashok-kumar140 <ashok.1@iitj.ac.in>
2024-05-27 03:42:14 +0530

    cors path updated

--- server/index.js
+++ server/index.js
@@ -17,12 +17,7 @@ dbConnect();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
-);
+app.use(cors());
 
 //routes
 app.use("/api/v1/auth", userRoutes);
